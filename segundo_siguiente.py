a49c82afb4f3603b437dc0bb4ad4b1f2c37ba5dc
Codg24 <carlos.diaz@iteso.mx>
2025-09-12 07:50:11 -0600

    Primera solución

--- segundo_siguiente.py
+++ segundo_siguiente.py
@@ -1,15 +1,38 @@
 """
-Inserta el encabezado aquí y escribe tu código abajo
+Calcular la hora correspondiente al siguiente segundo
 """
 
 # Declaraciones
-CONSTANTE = valor
+max_hora = 24
+max_minutos = 60
+max_segundos = 60
 
 # Entradas
-entrada = input()
+print("Hora inicial: ")
+hora = int(input("Hora: "))
+minuto = int(input("Minuto: "))
+segundo = int(input("Segundo: "))
 
 # Proceso
-salida = entrada
+if hora < max_hora and minuto < max_minutos and segundo < max_segundos:
+    segundo = segundo + 1
+    if segundo == 60:
+        segundo = 0
+        minuto = minuto +1
+        if minuto == 60:
+            minuto = 0
+            hora = hora +1
+            if hora == 24:
+                hora = 0
 
-# Salidas
-print(salida)
+    # Salidas
+    print("Hora final: ")
+    print("Hora:", hora)
+    print("Minuto:", minuto)
+    print("Segundo:", segundo)
+
+else:
+    #Salidas
+    print("Ingrese valores validos")
+
+
